f19beff4c81257a1ca177ad2255333cf5a80436b
oJaciel <jacielhbarichellO@gmail.com>
2025-07-15 07:26:40 -0300

    Implementação Métodos Autenticação
    Implementados os métodos para a autenticação do usuário.

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:chat/core/models/chat_user.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
@@ -6,21 +8,48 @@ import 'package:chat/core/services/auth/auth_service.dart';
 class AuthMockService implements AuthService {
   static Map<String, ChatUser> _users = {};
   static ChatUser? _currentUser;
+  static MultiStreamController<ChatUser?>? _controller;
+  static final _userStream = Stream<ChatUser?>.multi((controller) {
+    _controller = controller;
+    _updateUser(null);
+  });
 
   ChatUser? get currentUser {
     return _currentUser;
   }
 
-  Stream<ChatUser?> get userChanges {}
+  Stream<ChatUser?> get userChanges {
+    return _userStream;
+  }
 
   Future<void> signup(
     String name,
     String email,
     String password,
     File image,
-  ) async {}
+  ) async {
+    final newUser = ChatUser(
+      id: Random().nextDouble().toString(),
+      name: name,
+      email: email,
+      imageURL: image.path,
+    );
 
-  Future<void> login(String email, String password) async {}
+    _users.putIfAbsent(email, () => newUser);
 
-  Future<void> logout() async {}
+    _updateUser(newUser);
+  }
+
+  Future<void> login(String email, String password) async {
+    _updateUser(_users[email]);
+  }
+
+  Future<void> logout() async {
+    _updateUser(null);
+  }
+
+  static void _updateUser(ChatUser? user) {
+    _currentUser = user;
+    _controller?.add(_currentUser);
+  }
 }
