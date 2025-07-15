41fe244575d8a1101a9409de7c40e53f73135cf3
oJaciel <jacielhbarichellO@gmail.com>
2025-07-15 20:24:14 -0300

    Estrutura Tela de Chat
    Criada a tela de chat, e seus componentes. Adicionada a estrutura inicial da tela de Chat.

---
+++ lib/components/messages.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Messages extends StatelessWidget {
+  const Messages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: 
+    Text('Mensagens'),);
+  }
+}
