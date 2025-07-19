530d60c2661f1c98f380126e33440b1a905a5c03
oJaciel <jacielhbarichellO@gmail.com>
2025-07-19 20:25:59 -0300

    Enviando Nova Mensagem
    Implementado o componente de nova mensagem,  agora podendo enviar mensagens.
    
    Corrigido bug que fazia com que a ordem das mensagens ficassem ao contrário.

--- lib/core/services/chat/chat_mock_service.dart
+++ lib/core/services/chat/chat_mock_service.dart
@@ -6,32 +6,7 @@ import 'package:chat/core/models/chat_user.dart';
 import 'package:chat/core/services/chat/chat_service.dart';
 
 class ChatMockService implements ChatService {
-  static final List<ChatMessage> _msgs = [
-    ChatMessage(
-      id: '1',
-      text: 'Bom dia',
-      createdAt: DateTime.now(),
-      userId: '123',
-      userName: 'Lucas',
-      userImageURL: 'assets/images/avatar.png',
-    ),
-    ChatMessage(
-      id: '2',
-      text: 'Bom dia. Será que chove hoje?',
-      createdAt: DateTime.now(),
-      userId: '456',
-      userName: 'Clara',
-      userImageURL: 'assets/images/avatar.png',
-    ),
-    ChatMessage(
-      id: '3',
-      text: 'A previsão indica que sim!',
-      createdAt: DateTime.now(),
-      userId: '123',
-      userName: 'Lucas',
-      userImageURL: 'assets/images/avatar.png',
-    ),
-  ];
+  static final List<ChatMessage> _msgs = [];
   static MultiStreamController<List<ChatMessage>>? _controller;
   static final _msgsStream = Stream<List<ChatMessage>>.multi((controller) {
     _controller = controller;
@@ -53,7 +28,7 @@ class ChatMockService implements ChatService {
     );
 
     _msgs.add(newMessage);
-    _controller?.add(_msgs);
+    _controller?.add(_msgs.reversed.toList());
     return newMessage;
   }
 }
