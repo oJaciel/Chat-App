fc617711b2c657e4fcf3f73636465b21b90f69fc
oJaciel <jacielhbarichellO@gmail.com>
2025-07-20 20:47:41 -0300

    Serviço de Notificações
    Criado modelo de notificações.
    
    Criado serviço de notificações, usando Provider. Ainda sem implementação com Firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:chat/core/services/notification/chat_notification_service.dart';
 import 'package:chat/pages/auth_or_app_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +11,18 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ChatNotificationService()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: AuthOrAppPage(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: AuthOrAppPage(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
