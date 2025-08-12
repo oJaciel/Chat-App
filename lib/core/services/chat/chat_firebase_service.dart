96df62348e40c13425b70562f2471e0024b02fef
oJaciel <jacielhbarichellO@gmail.com>
2025-08-11 21:21:17 -0300

    Salvando Mensagem no Banco
    Criado serviço para tratar as mensagens no Firebase. Definido serviço como padrão.
    
    Implementado método para salvar as mensagens no Firestore.

---
+++ lib/core/services/chat/chat_firebase_service.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'package:chat/core/models/chat_message.dart';
+import 'package:chat/core/models/chat_user.dart';
+import 'package:chat/core/services/chat/chat_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatFirebaseService implements ChatService {
+  Stream<List<ChatMessage>> messagesStream() {
+    return Stream<List<ChatMessage>>.empty();
+  }
+
+  Future<ChatMessage?> save(String text, ChatUser user) async {
+    final store = FirebaseFirestore.instance;
+
+    //ChatMessage para Map<String, dynamic>
+    final docRef = await store.collection('chat').add({
+      'text': text,
+      'createdAt': DateTime.now().toIso8601String(),
+      'userId': user.id,
+      'userName': user.name,
+      'userImageURL': user.imageURL,
+    });
+
+    final doc = await docRef.get();
+    final data = doc.data()!;
+
+    //Map<String, dynamic> para ChatMessage
+
+    return ChatMessage(
+      id: doc.id,
+      text: data['text'],
+      createdAt: DateTime.parse(data['createdAt']),
+      userId: data['userId'],
+      userName: data['userName'],
+      userImageURL: data['userImageURL'],
+    );
+  }
+}
