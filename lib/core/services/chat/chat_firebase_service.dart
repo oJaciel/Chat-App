af70fabeb11af4d27ce4453fea83a0fd926377e1
oJaciel <jacielhbarichellO@gmail.com>
2025-08-12 21:34:13 -0300

    Carregando Mensagens + Refatoração
    Criado método para carregas as mensagens do banco de dados.
    
    Feita refatoração, para reaproveitamento de código nas interações de conversão de dados do Firebase.

--- lib/core/services/chat/chat_firebase_service.dart
+++ lib/core/services/chat/chat_firebase_service.dart
@@ -6,33 +6,67 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatFirebaseService implements ChatService {
   Stream<List<ChatMessage>> messagesStream() {
-    return Stream<List<ChatMessage>>.empty();
+    final store = FirebaseFirestore.instance;
+    final snapshots =
+        store
+            .collection('chat')
+            .withConverter(
+              fromFirestore: _fromFirestore,
+              toFirestore: _toFirestore,
+            )
+            .snapshots();
+
+    return snapshots.map((snapshot) {
+      return snapshot.docs.map((doc) {
+        return doc.data();
+      }).toList();
+    });
   }
 
   Future<ChatMessage?> save(String text, ChatUser user) async {
     final store = FirebaseFirestore.instance;
 
-    //ChatMessage para Map<String, dynamic>
-    final docRef = await store.collection('chat').add({
-      'text': text,
-      'createdAt': DateTime.now().toIso8601String(),
-      'userId': user.id,
-      'userName': user.name,
-      'userImageURL': user.imageURL,
-    });
+    final msg = ChatMessage(
+      id: '',
+      text: text,
+      createdAt: DateTime.now(),
+      userId: user.id,
+      userName: user.name,
+      userImageURL: user.imageURL,
+    );
+
+    final docRef = await store
+        .collection('chat')
+        .withConverter(fromFirestore: _fromFirestore, toFirestore: _toFirestore)
+        .add(msg);
 
     final doc = await docRef.get();
-    final data = doc.data()!;
+    return doc.data()!;
+  }
 
-    //Map<String, dynamic> para ChatMessage
+  //ChatMessage para Map<String, dynamic>
+  Map<String, dynamic> _toFirestore(ChatMessage msg, SetOptions? options) {
+    return {
+      'text': msg.text,
+      'createdAt': msg.createdAt.toIso8601String(),
+      'userId': msg.userId,
+      'userName': msg.userName,
+      'userImageURL': msg.userImageURL,
+    };
+  }
 
+  //Map<String, dynamic> para ChatMessage
+  ChatMessage _fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> doc,
+    SnapshotOptions? options,
+  ) {
     return ChatMessage(
       id: doc.id,
-      text: data['text'],
-      createdAt: DateTime.parse(data['createdAt']),
-      userId: data['userId'],
-      userName: data['userName'],
-      userImageURL: data['userImageURL'],
+      text: doc['text'],
+      createdAt: DateTime.parse(doc['createdAt']),
+      userId: doc['userId'],
+      userName: doc['userName'],
+      userImageURL: doc['userImageURL'],
     );
   }
 }
